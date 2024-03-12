03efc0c809fd1f7b7de6bae7e893fe33a69885af
chinzorig17 <breakp70@yahoo.com>
2024-03-12 20:14:30 +0900

    Initialize partial-hydration-test

---
+++ src/pages/about.js
@@ -0,0 +1,48 @@
+// "use client"
+
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import * as sections from "../components/sections"
+import Fallback from "../components/fallback"
+import ASQ from "../components/about-static-query"
+
+export default function About(props) {
+  const { aboutPage } = props.data
+
+  return (
+    <Layout {...aboutPage}>
+      <ASQ />
+      {aboutPage.blocks.map((block) => {
+        const { id, blocktype, ...componentProps } = block
+        const Component = sections[blocktype] || Fallback
+        return <Component key={id} {...componentProps} />
+      })}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    aboutPage {
+      id
+      title
+      description
+      image {
+        id
+        url
+      }
+      blocks: content {
+        id
+        blocktype
+        ...AboutHeroContent
+        ...AboutStatListContent
+        ...HomepageProductListContent
+        ...AboutLeadershipContent
+        ...HomepageBenefitListContent
+        ...AboutLogoListContent
+        ...HomepageCtaContent
+      }
+    }
+  }
+`
